2261d7be14083ec775a22cc00a684c1da65d3e1b
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-01-27 13:43:38 +0100

    Fixxed setLastEntity Cast Exception

--- src/main/java/de/hglabor/plugins/ffa/gamemechanics/LastHitDetection.java
+++ src/main/java/de/hglabor/plugins/ffa/gamemechanics/LastHitDetection.java
@@ -18,11 +18,13 @@ public class LastHitDetection implements Listener {
             LastHitInformation lastHitInformation = attacker.getLastHitInformation();
             lastHitInformation.setPlayerTimeStamp(System.currentTimeMillis());
             lastHitInformation.setLastPlayer((Player) event.getEntity());
+            lastHitInformation.setEntityTimeStamp(System.currentTimeMillis());
+            lastHitInformation.setLastEntity((LivingEntity) event.getEntity());
         } else if (event.getDamager() instanceof Player && event.getEntity() instanceof LivingEntity) {
             KitPlayer attacker = PlayerList.getInstance().getKitPlayer((Player) event.getDamager());
             LastHitInformation lastHitInformation = attacker.getLastHitInformation();
             lastHitInformation.setEntityTimeStamp(System.currentTimeMillis());
-            lastHitInformation.setLastEntity((Player) event.getEntity());
+            lastHitInformation.setLastEntity((LivingEntity) event.getEntity());
         }
     }
 }
